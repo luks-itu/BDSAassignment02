dfef9dd288da2aba3e121d6947240071b87627fa
Gustav Müller Christoffersen <guch@itu.dk>
2021-09-20 18:33:05 +0200

    Created tests and implemented required functionality - 20/09-2021

--- AssignmentLibrary.Tests/LibraryTests.cs
+++ AssignmentLibrary.Tests/LibraryTests.cs
@@ -8,19 +8,24 @@ namespace AssignmentLibrary.Tests
         [Fact]
         public void StudentStatusSomething()
         {
-
+            Student st = new Student(01, "Gustav","Müller", DateTime.Now.AddMonths(-1), DateTime.Now.AddMonths(3), DateTime.Now.AddMonths(2));
+            Assert.Equal(st.getStatus(), Stats.Active);
         }
 
         [Fact]
-        public void StudentIDSomething()
+        public void CompareTwoImmutableStudents()
         {
-
+            ImmutableStudent st1 = new ImmutableStudent(01, "Gustav","Müller", DateTime.MinValue, DateTime.MaxValue, DateTime.MaxValue);
+            ImmutableStudent st2 = new ImmutableStudent(01, "Gustav","Müller", DateTime.MinValue, DateTime.MaxValue, DateTime.MaxValue);
+            var validation = (st1 == st2);
+            Assert.True(validation);
         }
-
+        
         [Fact]
-        public void StudentToStringSomething()
+        public void TestToString()
         {
-            
+            ImmutableStudent st = new ImmutableStudent(01, "Gustav","Müller", DateTime.MinValue, DateTime.MaxValue, DateTime.MaxValue);
+            Assert.Equal("ImmutableStudent { id = 1, GivenName = Gustav, Surname = Müller, status = Active, StartDate = 01/01/0001 00:00:00, endDate = 31/12/9999 23:59:59, graduationDate = 31/12/9999 23:59:59 }", st.ToString());
         }
     }
 }
--- AssignmentLibrary/Student.cs
+++ AssignmentLibrary/Student.cs
@@ -4,6 +4,92 @@ namespace AssignmentLibrary
 {
     public class Student
     {
-      
+        private int id {get; init;}
+        private string GivenName {get; set;}
+        private string Surname {get; set;}
+        private Stats status {get => getStatus();}
+        public  DateTime StartDate {get; set;}
+        private DateTime endDate {get; set;}
+        private DateTime graduationDate {get; set;}
+
+        public Student (int id, string GivenName, string Surname, DateTime startDate, DateTime endDate, DateTime graduationDate) {
+            this.id = id;
+            this.GivenName = GivenName;
+            this.Surname = Surname;
+            this.StartDate = startDate;
+            this.endDate = endDate;
+            this.graduationDate = graduationDate;
+        }
+
+
+        public Stats getStatus() {
+            if (DateTime.Compare(endDate, graduationDate) < 0) 
+            {
+                return Stats.Dropout; 
+            }
+            else if (DateTime.Compare(graduationDate, DateTime.Now) < 0) 
+            {
+                return Stats.Graduated;
+            }
+            else if (DateTime.Compare(StartDate, DateTime.Now) > 0 ) 
+            {
+                return Stats.New;
+            }
+            else 
+            {
+                return Stats.Active;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Id: {0}, Name: {1} {2}, Status: {3}, StartDate: {4}, EndDate: {5}, GraduationDate: {6}", id, GivenName,Surname,status,StartDate.ToString(),endDate.ToString(),graduationDate.ToString());
+        }
+    }
+
+    public record ImmutableStudent {
+        public int id {get; init;}
+        public string GivenName {get; init;}
+        public string Surname {get; init;}
+        public Stats status {get => getStatus();}
+        public  DateTime StartDate {get; init;}
+        public DateTime endDate {get; init;}
+        public DateTime graduationDate {get; init;}
+
+
+        public ImmutableStudent (int id, string GivenName, string Surname, DateTime startDate, DateTime endDate, DateTime graduationDate) {
+            this.id = id;
+            this.GivenName = GivenName;
+            this.Surname = Surname;
+            this.StartDate = startDate;
+            this.endDate = endDate;
+            this.graduationDate = graduationDate;
+        }
+
+        public Stats getStatus() {
+            if (DateTime.Compare(endDate, graduationDate) < 0) 
+            {
+                return Stats.Dropout; 
+            }
+            else if (DateTime.Compare(graduationDate, DateTime.Now) < 0) 
+            {
+                return Stats.Graduated;
+            }
+            else if (DateTime.Compare(StartDate, DateTime.Now) > 0 ) 
+            {
+                return Stats.New;
+            }
+            else 
+            {
+                return Stats.Active;
+            }
+        }
+    }
+
+    
+    
+    public enum Stats 
+    {
+        New, Active, Dropout, Graduated
     }
 }
