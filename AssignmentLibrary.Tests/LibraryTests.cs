5eb0ae846420a790b0f750da26742d14386cb93d
luks <luks@itu.dk>
2021-09-20 13:49:21 +0200

    added files for ex 1

---
+++ AssignmentLibrary.Tests/LibraryTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace AssignmentLibrary.Tests
+{
+    public class StudentTests
+    {
+        [Fact]
+        public void StudentStatusSomething()
+        {
+
+        }
+
+        [Fact]
+        public void StudentIDSomething()
+        {
+
+        }
+
+        [Fact]
+        public void StudentToStringSomething()
+        {
+            
+        }
+    }
+}
---
+++ AssignmentLibrary/Student.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AssignmentLibrary
+{
+    public class Student
+    {
+      
+    }
+}
